d8c34e3eea9fabfcb70143a5bc4917a60db30be3
saradaperuboina <sarada.peruboina@gmail.com>
2017-04-27 22:37:15 -0500

    e-commerce
    adding the project

---
+++ wpl/Client/js/controllers/mybids.js
@@ -0,0 +1,38 @@
+angular.module('app')
+    .controller('MyBidsCtrl', function ($scope, $auth, toastr, $timeout, BidService, NgTableParams,$location) {
+        var getBids = function() {
+            BidService.getMyBids().then(function (data) {
+                // params.total(data); // recal. page nav controls
+                $scope.dataSet = data.data;
+                $timeout(function () {
+                    $scope.tableParams = new NgTableParams({
+                        // initial sort order
+                        sorting: {createdAt: "desc"}
+                    }, {
+                        dataset: $scope.dataSet
+                    });
+                });
+            })
+        };
+
+        $scope.delete = function (data) {
+            BidService.deleteBidbyId(data._id)
+                .then(function () {
+                    toastr.success("The bid has been successfully deleted");
+                    // $scope.view(datar);
+                    getBids();
+                }, function (error) {
+                    toastr.error(error.data.message, error.status);
+                });
+        };
+
+
+      $scope.convert= function(date){
+            return moment(date).fromNow();
+        };
+
+        $scope.view= function(data){
+            $location.path('/user/bid/'+data._id);
+        };
+        getBids();
+    });
